af8666056e0c2e5d0c662c4eda9b47143ea4a9cb
DevAbdone <adelopoadekunle@gmail.com>
2024-09-10 15:40:43 +0100

    Fixed the states panel

--- src/views/panelView.js
+++ src/views/panelView.js
@@ -1,3 +1,13 @@
+// Import images
+import taskImg from "../images/Task.png";
+import taskActiveImg from "../images/TaskActive.png";
+import completedImg from "../images/completed.png";
+import completedActiveImg from "../images/completedActive.png";
+import pendingImg from "../images/pending.png";
+import pendingActiveImg from "../images/pendingActive.png";
+import deleteImg from "../images/delete.png";
+import deleteActiveImg from "../images/deleteActive.png";
+
 class PanelView {
   #parentElement = document.querySelector(".states");
 
@@ -11,16 +21,35 @@ class PanelView {
 
       allButtons.forEach((button) => {
         const image = button.querySelector("img");
+        const activeImage = button.dataset.active;
+        const defaultImage = button.dataset.default;
+
+        // Dynamically set the src based on the imported images
         if (button === clicked) {
-          image.src = button.dataset.active;
+          image.src = this.getImageSrc(activeImage);
           clicked.classList.add("active");
           return;
         }
 
-        image.src = button.dataset.default;
+        image.src = this.getImageSrc(defaultImage);
         button.classList.remove("active");
       });
     });
+  }
+
+  getImageSrc(imageName) {
+    const imageMap = {
+      "src/images/Task.png": taskImg,
+      "src/images/TaskActive.png": taskActiveImg,
+      "src/images/completed.png": completedImg,
+      "src/images/completedActive.png": completedActiveImg,
+      "src/images/pending.png": pendingImg,
+      "src/images/pendingActive.png": pendingActiveImg,
+      "src/images/delete.png": deleteImg,
+      "src/images/deleteActive.png": deleteActiveImg,
+    };
+
+    return imageMap[imageName] || ""; // Default to empty string if not found
   }
 }
 
